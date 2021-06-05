c61c6f01515036d94dfee16efafed1ec38a49516
nidamirza9 <61187406+nidamirza9@users.noreply.github.com>
2021-06-05 12:55:35 +0530

    Task-6

---
+++ DAY_6/checkout.php
@@ -0,0 +1,11 @@
+<?php 
+include 'includes/header.php'?>
+     <div class="main">
+      <div class="shop_top">
+		<div class="container">
+			<h4 class="title">Shopping cart is empty</h4>
+			<p class="cart">You have no items in your shopping cart.<br>Click<a href="index.html"> here</a> to continue shopping</p>
+	     </div>
+	   </div>
+	  </div>
+		<?php require 'includes/footer.php' ?>
